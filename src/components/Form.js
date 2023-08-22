85938c86bd229f893c84a53d5327b3ed2695cdfe
parmanandkumawat1994 <parmanandkumawat.vhits@gmail.com>
2023-08-22 19:00:42 +0530

    add form

---
+++ src/components/Form.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Form = () => {
+  return (
+    <div>
+      <input type="text" placeholder='Name'  />
+      <input type="text" placeholder='adress'  />
+
+
+      <button>Submit </button>
+    </div>
+  );
+}
+
+export default Form;
